da3d0a5e2b3021d779cc4d04b5555578818a03a0
mieum12 <qpdlqltb1215@gmail.com>
2024-02-09 15:30:33 +0900

    feat: 로그아웃 구현

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,11 +1,18 @@
 import Link from 'next/link';
 import classes from './main-navigation.module.css';
-import {useSession} from "next-auth/react";
+import {signOut, useSession} from "next-auth/react";
 
 function MainNavigation() {
   // session의 활성 상태를 나타내는 세션 객체, 로그인된 상태인지 아닌지 확인하는 요소
   const {data: session, loading} = useSession()
   console.log(session, loading)
+
+  function logoutHandler() {
+    // useSession을 사용하기 때문에 session에 변화가 있을 때마다
+    // 컴포넌트가 자동으로 업데이트 되어서
+    // await을 쓸 필요없다
+    signOut() // 쿠키와 세션 정보를 삭제
+  }
 
   return (
     <header className={classes.header}>
@@ -27,7 +34,7 @@ function MainNavigation() {
           )}
           {session && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
