aeddbb6bd3824e14603dcdcdb9290cc296fb9cca
mieum12 <qpdlqltb1215@gmail.com>
2024-02-09 13:36:11 +0900

    feat: create user API 연결완료

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,24 +1,74 @@
-import { useState } from 'react';
+import React, {useRef, useState} from 'react';
 import classes from './auth-form.module.css';
 
+async function createUser(email, password) {
+
+  // 요청 전송
+  // 응답을 대기하고 처리한다
+  console.log('요청 이제 시작..💙')
+  const response = await fetch('/api/auth/signup', {
+    method: 'POST',
+    body: JSON.stringify({email,password}),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  console.log('요청 끝남..💙')
+
+  console.log('======> response: ',response)
+
+  const data = await response.json()
+
+  if (!response.ok) {
+    throw new Error(`'에러가 생겼음!!',data.message` || 'error!!!!')
+  }
+  console.log('data:', data)
+  return data
+}
+
 function AuthForm() {
+  const emailInputRef = useRef()
+  const passwordInputRef = useRef()
+
   const [isLogin, setIsLogin] = useState(true);
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
 
+  async function submitHandler(e) {
+    e.preventDefault();
+
+    // ref가 있으니 데이터를 가져올 수 있다
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // 이 곳에 유효성 검사 넣을 수도 있다
+
+    if (isLogin) {
+      // 사용자를 로그인시킴
+    } else {
+      try {
+        // create mode 라면 사용자를 생성하게 한다
+        const result = await createUser(enteredEmail, enteredPassword)
+        console.log('사용자 생성!',result)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -5,10 +5,17 @@ import {connectToDatabase} from "@/lib/db";
 import {hashPassword} from "@/lib/auth";
 
 async function handler(req, res) {
+  // post 일 때만 사용자를 생성하는 로직이기 때문에 post가 아니라면 끝내준다
+  if (req.method !== 'POST') {
+    console.log('not post!!!')
+    return
+  }
 
+  console.log('yes post!!!1')
   // 유효한 데이터가 있을때만 작업하기 위해 들어오는 데이터 먼저 구성하기
   const data = req.body
   const { email, password } = data
+  console.log('email&pw ----------', email, password)
 
   // 유효한 데이터인지 검증 -> 검증 되면 데이터베이스(컬렉션)에 저장할 대상이 된다
   if (
@@ -26,20 +33,25 @@ async function handler(req, res) {
     return
   }
 
+  console.log('유효성 검증은 통과')
 
   const client = await connectToDatabase()
+  console.log('client', client)
 
   const db = client.db()
-
-  // users라는 컬렉션 생성하기 (사용자 추가)
+  console.log('db', db)
 
   // 비번은 저장할 때 암호화하기
-  const hashedPassword = hashPassword(password)
+  const hashedPassword = await hashPassword(password)
+  console.log('hashedPassword: ',hashedPassword)
 
+  // users라는 컬렉션 생성하기 (사용자 추가)
   const result = await db.collection('users').insertOne({
     email: email,
     password: hashedPassword
   })
+
+  console.log('result: ',result)
 
   res.status(201).json({message:'Create user!'})
 }
