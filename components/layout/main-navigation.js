a87612c176f48bec2dbe0e278fc684a41601031c
mieum12 <qpdlqltb1215@gmail.com>
2024-02-09 15:25:05 +0900

    fix: useSession 에러 해결 및 네비게이션 바 수정

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
-
 import classes from './main-navigation.module.css';
+import {useSession} from "next-auth/react";
 
 function MainNavigation() {
+  // session의 활성 상태를 나타내는 세션 객체, 로그인된 상태인지 아닌지 확인하는 요소
+  const {data: session, loading} = useSession()
+  console.log(session, loading)
+
   return (
     <header className={classes.header}>
       <Link href='/'>
@@ -10,15 +14,22 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
+          {/*불러오는 중이나 세션이 있을때는 로그인을 표시하고싶지 않다*/}
+          {!session && !loading && (
+            <li>
             <Link href='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href='/profile'>Profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout/layout";
+import {SessionProvider} from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+
   )
 }
