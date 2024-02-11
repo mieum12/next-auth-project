b07088bd8cc71a2ff9209e1add938b22fa1efce9
mieum12 <qpdlqltb1215@gmail.com>
2024-02-11 16:41:51 +0900

    feat: 비번 변경 등을 위한 API 라우트 보호 로직 구현

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import {SessionProvider} from "next-auth/react";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
+    // 내부 코드를 최적화 해서 useSession이 보내는
+    // 추가적인 HTTP 요청 (세션 유효성 검사 요청)을 건너뛰게 함
     <SessionProvider session={session}>
       <Layout>
         <Component {...pageProps} />
